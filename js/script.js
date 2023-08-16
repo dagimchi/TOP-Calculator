528c6c11102778b7623c672c9f1b275a7830dba7
Hae <haechoi92@yahoo.com>
2023-08-16 11:25:08 -0700

    Add logic for chaining different operations

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 let num1, operator, num2;
 let displayNum;
+let pairEvaluation;
 let numbers = document.querySelectorAll(".target-num");
 let operators = document.querySelectorAll(".target-operator");
 let display = document.querySelector(".screen");
 let equal = document.querySelector(".equal");
 let flag = false;
+let firstPairValue;
 
 operators.forEach((button) => {
   button.addEventListener("click", () => {
     flag = true;
     if (button.classList.contains("divide")) {
       operator = "/";
+      if (parseInt(num1) && parseInt(num2)) {
+        num1 = firstPairValue;
+      }
     } else if (button.classList.contains("multiply")) {
       operator = "*";
+      if (parseInt(num1) && parseInt(num2)) {
+        num1 = firstPairValue;
+      }
     } else if (button.classList.contains("subtract")) {
       operator = "-";
+      if (parseInt(num1) && parseInt(num2)) {
+        num1 = firstPairValue;
+      }
     } else if (button.classList.contains("add")) {
       operator = "+";
+      if (parseInt(num1) && parseInt(num2)) {
+        num1 = firstPairValue;
+      }
     }
   });
 });
@@ -29,6 +43,9 @@ numbers.forEach((button) => {
     } else if (flag === true) {
       display.innerText = button.textContent;
       num2 = display.innerText;
+      let numConverted1 = parseInt(num1);
+      let numConverted2 = parseInt(num2);
+      firstPairValue = operate(operator, numConverted1, numConverted2);
       flag = "continue";
     } else if (flag === "continue") {
       display.innerText += button.textContent;
@@ -40,8 +57,8 @@ numbers.forEach((button) => {
 equal.addEventListener("click", () => {
   let convertedNum1 = parseInt(num1);
   let convertedNum2 = parseInt(num2);
-  let answer = operate(operator, convertedNum1, convertedNum2);
-  display.innerText = answer;
+  pairEvaluation = operate(operator, convertedNum1, convertedNum2);
+  display.innerText = pairEvaluation;
 });
 
 function operate(operator, num1, num2) {
